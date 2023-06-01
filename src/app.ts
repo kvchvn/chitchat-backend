5444c6f6d479a99819321a1a5a8c0c2e660801e0
Anton Kachan <aa.kvchvn@gmail.com>
2023-06-01 18:14:17 +0300

    feat(app): common error handling for not allowed requests

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 import path from 'path';
+import { REQUEST_NOT_ALLOWED } from './constants/index.ts';
 import { getDirname } from './helpers/index.ts';
 import { authRouter } from './routers/index.ts';
 
@@ -10,3 +12,5 @@ export const app = express();
 app.use('/static', express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(authRouter);
+
+app.all('*', (_, res) => res.status(StatusCodes.NOT_FOUND).send(REQUEST_NOT_ALLOWED));
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export { MONGO_ENTITY_ALREADY_EXISTS_CODE } from './common.ts';
+export { REQUEST_NOT_ALLOWED } from './errors.ts';
