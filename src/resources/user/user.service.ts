4c4c128acb5c025ba754fbbe4d44b1f2e84a1aa5
Anton Kachan <aa.kvchvn@gmail.com>
2023-06-13 17:34:28 +0300

    feat(service): create UserService class

---
+++ src/resources/user/user.service.ts
@@ -0,0 +1,22 @@
+import { NotFoundError } from '../../errors/app-error.ts';
+import { User } from '../../types/user.ts';
+import { UserModel } from './user.model.ts';
+
+class UserService {
+  async getUserById(id: string) {
+    const user = await UserModel.findById(id).exec();
+    if (!user) {
+      throw new NotFoundError({ entity: 'user', params: { id } });
+    }
+
+    return user;
+  }
+
+  async createNewUser(userData: User) {
+    const user = new UserModel(userData);
+    await UserModel.create(user);
+    return user;
+  }
+}
+
+export const userService = new UserService();
