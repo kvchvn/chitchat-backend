ab87e7830254dfb40adf81eb0002d3f238c8ec13
Anton Kachan <aa.kvchvn@gmail.com>
2023-06-13 17:22:10 +0300

    feat(constants): define some new constants

---
+++ src/constants/errors.ts
@@ -0,0 +1,3 @@
+export const MONGO_ENTITY_ALREADY_EXISTS_CODE = 11000;
+
+export const REQUEST_NOT_ALLOWED = 'Неверный метод или адрес запроса.';
--- src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export { MONGO_ENTITY_ALREADY_EXISTS_CODE } from './common.ts';
-export { REQUEST_NOT_ALLOWED } from './errors.ts';
