4eee3f998a9187572977211dff30e4064f2526a7
Anton Kachan <aa.kvchvn@gmail.com>
2023-09-13 19:17:55 +0300

    feat(errors): create some extended Error classes

---
+++ src/types/global.ts
@@ -0,0 +1,7 @@
+export type Entities = 'user';
+
+export type ErrorResponse = {
+  status: number;
+  message: string;
+  issues?: string[];
+};
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { Entities, ErrorResponse } from './global';
