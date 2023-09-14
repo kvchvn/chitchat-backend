75913a609039936a2a5a690c7c60d7565b124b86
Anton Kachan <aa.kvchvn@gmail.com>
2023-09-14 13:14:59 +0300

    feat(UsersRouter): add routes, narrow types of request, response

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,11 +1,44 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { usersController } from '../controllers';
-import { validateUserId } from '../validation';
+import { SuccessResponse } from '../types';
+import { friendRequestSchema, validate, validateUserId } from '../validation';
 
 export const usersRouter = express.Router();
 
-usersRouter.get('/:userId', validateUserId(), async (req, res) => {
-  const allUsersExceptOneself = await usersController.getAllUsersExceptOneself(req.params.userId);
-  res.status(StatusCodes.OK).send(allUsersExceptOneself);
-});
+usersRouter.get(
+  '/:userId',
+  validateUserId(),
+  async (req: Request<{ userId: string }>, res: Response<SuccessResponse>) => {
+    const allUsersExceptOneself = await usersController.getAllUsersExceptOneself(req.params.userId);
+    res.status(StatusCodes.OK).send({ ok: true, data: allUsersExceptOneself });
+  }
+);
+
+usersRouter.get(
+  '/:userId/friends',
+  validateUserId(),
+  async (req: Request<{ userId: string }>, res: Response<SuccessResponse>) => {
+    const friendsAndFriendRequests = await usersController.getFriendsAndFriendRequests(
+      req.params.userId
+    );
+    res.status(StatusCodes.OK).send({ ok: true, data: friendsAndFriendRequests });
+  }
+);
+
+usersRouter.post(
+  '/friend-request',
+  validate(friendRequestSchema),
+  async (
+    req: Request<unknown, unknown, { senderId: string; receiverId: string }>,
+    res: Response<SuccessResponse>,
+    next: NextFunction
+  ) => {
+    try {
+      await usersController.sendFriendRequest(req.body.senderId, req.body.receiverId);
+    } catch (err) {
+      next(err);
+    }
+    res.status(StatusCodes.ACCEPTED).send({ ok: true });
+  }
+);
