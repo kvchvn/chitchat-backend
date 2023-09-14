3129ec092e711b173757ebae2b726be531ef5684
Anton Kachan <aa.kvchvn@gmail.com>
2023-09-14 15:13:09 +0300

    feat(errors): add condition to prismaErrorHandler

--- src/errors/prisma-error-handler.ts
+++ src/errors/prisma-error-handler.ts
@@ -2,6 +2,10 @@ import { isPrismaKnownError } from '../types';
 import { BadRequestError } from './app-errors';
 
 export const prismaErrorHandler = (err: unknown) => {
+  if (err instanceof BadRequestError) {
+    throw err;
+  }
+
   const message = `Error when accessing the database [${
     isPrismaKnownError(err) ? err.code : 'unknown'
   }]`;
