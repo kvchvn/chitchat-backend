1d43edf66f338752254ee0aa5e5832fa18b172e0
Anton Kachan <aa.kvchvn@gmail.com>
2023-09-20 17:52:07 +0300

    refactor(usersRouter): replace logic from usersRouter into UsersController

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,30 +1,66 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { usersService } from '../services';
+import { GetUsersResponse } from '../types';
 
 class UsersController {
-  async getUsers(userId: string) {
-    return await usersService.getUsers(userId);
+  async getUsers(
+    req: Request<{ userId: string }>,
+    res: Response<GetUsersResponse>,
+    next: NextFunction
+  ) {
+    try {
+      const users = await usersService.getUsers(req.params.userId);
+      res.status(StatusCodes.OK).send(users);
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async sendFriendRequest({ senderId, receiverId }: { senderId: string; receiverId: string }) {
-    return await usersService.sendFriendRequest({ senderId, receiverId });
+  async sendFriendRequest(
+    req: Request<unknown, unknown, { senderId: string; receiverId: string }>,
+    res: Response<void>,
+    next: NextFunction
+  ) {
+    try {
+      const { senderId, receiverId } = req.body;
+      await usersService.sendFriendRequest({ senderId, receiverId });
+      res.sendStatus(StatusCodes.NO_CONTENT);
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async respondToFriendRequest({
-    isAccepted,
-    senderId,
-    receiverId,
-  }: {
-    isAccepted: boolean;
-    senderId: string;
-    receiverId: string;
-  }) {
-    return isAccepted
-      ? await usersService.acceptFriendRequest({ senderId, receiverId })
-      : await usersService.refuseFriendRequest({ senderId, receiverId });
+  async respondToFriendRequest(
+    req: Request<unknown, unknown, { senderId: string; receiverId: string; isAccepted: boolean }>,
+    res: Response<void>,
+    next: NextFunction
+  ) {
+    const { senderId, receiverId, isAccepted } = req.body;
+    try {
+      if (isAccepted) {
+        await usersService.acceptFriendRequest({ senderId, receiverId });
+      } else {
+        await usersService.refuseFriendRequest({ senderId, receiverId });
+      }
+      res.sendStatus(StatusCodes.NO_CONTENT);
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async removeFromFriends({ userId, userFriendId }: { userId: string; userFriendId: string }) {
-    return await usersService.removeFromFriends({ userId, userFriendId });
+  async removeFromFriends(
+    req: Request<unknown, unknown, { userId: string; userFriendId: string }>,
+    res: Response<void>,
+    next: NextFunction
+  ) {
+    try {
+      const { userId, userFriendId } = req.body;
+      await usersService.removeFromFriends({ userId, userFriendId });
+      res.sendStatus(StatusCodes.NO_CONTENT);
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,7 +1,5 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import express from 'express';
 import { usersController } from '../controllers';
-import { GetUsersResponse } from '../types';
 import {
   friendRemovingSchema,
   friendRequestSchema,
@@ -12,66 +10,22 @@ import {
 
 export const usersRouter = express.Router();
 
-usersRouter.get(
-  '/:userId',
-  validateUserId(),
-  async (req: Request<{ userId: string }>, res: Response<GetUsersResponse>, next: NextFunction) => {
-    try {
-      const users = await usersController.getUsers(req.params.userId);
-      res.status(StatusCodes.OK).send(users);
-    } catch (err) {
-      next(err);
-    }
-  }
-);
+usersRouter.get('/:userId', validateUserId(), usersController.getUsers);
 
 usersRouter.post(
   '/friend-request',
   validate(friendRequestSchema),
-  async (
-    req: Request<unknown, unknown, { senderId: string; receiverId: string }>,
-    res: Response<void>,
-    next: NextFunction
-  ) => {
-    try {
-      await usersController.sendFriendRequest(req.body);
-      res.sendStatus(StatusCodes.NO_CONTENT);
-    } catch (err) {
-      next(err);
-    }
-  }
+  usersController.sendFriendRequest
 );
 
 usersRouter.post(
   '/friend-response',
   validate(friendResponseSchema),
-  async (
-    req: Request<unknown, unknown, { senderId: string; receiverId: string; isAccepted: boolean }>,
-    res: Response<void>,
-    next: NextFunction
-  ) => {
-    try {
-      await usersController.respondToFriendRequest(req.body);
-      res.sendStatus(StatusCodes.NO_CONTENT);
-    } catch (err) {
-      next(err);
-    }
-  }
+  usersController.respondToFriendRequest
 );
 
 usersRouter.delete(
   '/friend-removing',
   validate(friendRemovingSchema),
-  async (
-    req: Request<unknown, unknown, { userId: string; userFriendId: string }>,
-    res: Response<void>,
-    next: NextFunction
-  ) => {
-    try {
-      await usersController.removeFromFriends(req.body);
-      res.sendStatus(StatusCodes.NO_CONTENT);
-    } catch (err) {
-      next(err);
-    }
-  }
+  usersController.removeFromFriends
 );
