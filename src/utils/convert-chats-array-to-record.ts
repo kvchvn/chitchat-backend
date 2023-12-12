416b50cd4ffe981b11d63e5a2381a036aebc86f4
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-12 21:59:41 +0300

    feat(utils): implement another utility function

--- src/utils/convert-chats-array-to-record.ts
+++ src/utils/convert-chats-array-to-record.ts
@@ -1,12 +1,19 @@
 import { ChatsRecord, ExtendedChat } from '../types/responses';
+import { excludeUserByIdFromUsersArray } from './exclude-user-by-id-from-users-array';
 
-export const convertChatsArrayToRecord = (chats: ExtendedChat[]) => {
+export const convertChatsArrayToRecord = ({
+  chats,
+  userId,
+}: {
+  chats: ExtendedChat[];
+  userId: string;
+}) => {
   const chatsRecord: ChatsRecord = {};
 
   chats.forEach(({ id, isDisabled, messages, users, _count }) => {
     chatsRecord[id] = {
       isDisabled,
-      users,
+      users: excludeUserByIdFromUsersArray({ users, userId }),
       lastMessage: messages[0],
       unreadMessagesCount: _count.messages,
     };
