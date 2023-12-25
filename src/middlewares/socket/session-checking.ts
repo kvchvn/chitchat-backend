61e50770e9ef65498ef082e88031c750bdba7249
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-25 19:39:52 +0300

    feat(middleware): create session-checking middleware for socket

---
+++ src/middlewares/socket/session-checking.ts
@@ -0,0 +1,29 @@
+import { Socket } from 'socket.io';
+import { ExtendedError } from 'socket.io/dist/namespace';
+import { AuthorizationError } from '../../errors/app-errors';
+import { sessionService } from '../../services/session-service';
+
+export const sessionChecking = async (
+  socket: Socket,
+  next: (err?: ExtendedError | undefined) => void
+) => {
+  try {
+    const sessionToken: string | undefined = socket.handshake.auth.sessionToken;
+    const session = sessionToken ? await sessionService.getSessionByToken(sessionToken) : null;
+    if (sessionToken && session) {
+      next();
+    } else {
+      next(new AuthorizationError());
+    }
+  } catch (err) {
+    let message = 'Error occurred in session checking middleware: ';
+    if (err && typeof err === 'object' && 'message' in err) {
+      message += err.message;
+    } else {
+      message += '<no details>';
+    }
+    const extendedErr = { name: 'SessionCheckingMiddleware', message } satisfies ExtendedError;
+    console.log({ extendedErr });
+    next(extendedErr);
+  }
+};
