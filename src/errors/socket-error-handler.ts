46b372632b5158cef683ab84a90a7c0397db882f
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-28 10:54:56 +0300

    feat(socket-error-handler): emit 'error' event

--- src/errors/socket-error-handler.ts
+++ src/errors/socket-error-handler.ts
@@ -1,3 +1,15 @@
-export const socketErrorHandler = (err: unknown) => {
-  console.log("Error occurred in Socket's event:\n", JSON.stringify(err, null, 2));
+import { StatusCodes } from 'http-status-codes';
+import { ParsedError } from '../types/global';
+import { isParsedError } from '../types/guards';
+import { CustomSocket } from '../types/socket';
+
+export const socketErrorHandler = (err: unknown, socket: CustomSocket) => {
+  const error: ParsedError = isParsedError(err)
+    ? err
+    : {
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: 'Unknown server error.',
+      };
+  console.log('Socket Error Handler: ', error);
+  socket.emit('error', { ...error, message: error.message });
 };
