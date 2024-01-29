a7a4ebc4fb3557b64041529d7ad211b4c4224665
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-29 13:52:36 +0300

    refactor(utils): correct comment

--- src/utils/aggregate-messages-by-date-record.ts
+++ src/utils/aggregate-messages-by-date-record.ts
@@ -4,7 +4,7 @@ export const aggregateMessagesByDateRecord = (messages: Message[]) => {
   const messagesByDate: Record<string, Message[]> = {};
 
   messages.forEach((message) => {
-    // "+1" to get correct date
+    // "+1" to get correct month
     const month = message.createdAt.getMonth() + 1;
     const date = message.createdAt.getDate();
     const year = message.createdAt.getFullYear();
